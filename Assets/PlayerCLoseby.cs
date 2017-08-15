7caf0fe2c88c2cdd6b44094b872a9ec39aaa1e39
Jacques <jacqueslenormand@gmail.com>
2017-08-15 14:02:48 -0400

    eh

---
+++ Assets/PlayerCLoseby.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCLoseby : MonoBehaviour {
+    public GetKey playerGetKey;
+    public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 0.3f)
+        {
+            playerGetKey.KeyGot();
+        }
+	}
+}
